292ca2eb14184fa1d319b6c59b3d4909984a7832
Visar Gashi <visar@polymathlabs.co>
2025-06-26 04:29:44 +0000

    Updated ESLint configuration for TypeScript, bumped version to 2.1.0, and added new dependencies. Enhanced rule definitions and improved module exports.

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,52 +1,52 @@
-module.exports = {
+import tseslint from 'typescript-eslint';
+import polymathLabsNodeConfig from '@polymath_labs/eslint-config-node';
 
-    env: {
-        "es2021": true
-    },
-
-    parser: "@typescript-eslint/parser",
-
-    parserOptions: {
-        ecmaVersion: 13,
-        project: ['./tsconfig.json']
-    },
-
+export default [
+  polymathLabsNodeConfig,
+  {
+    name: 'polymath_labs/typescript/rules',
+    files: ['**/*.{ts,tsx,cts,mts}'],
     extends: [
-        "eslint:recommended",
-        "@polymath_labs/eslint-config-node",
-        "plugin:@typescript-eslint/recommended",
+      tseslint.configs.recommended,
     ],
-
-    plugins: [
-        "@typescript-eslint"
-    ],
-
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+    },
     rules: {
-        "@typescript-eslint/explicit-function-return-type": ["error"],
+      '@typescript-eslint/explicit-function-return-type': ['error'],
 
-        // note you must disable the base rule as it can report incorrect errors
-        "no-extra-parens": "off",
-        "@typescript-eslint/no-extra-parens": ["error"],
+      // note you must disable the base rule as it can report incorrect errors
+      // Use @stylistic/eslint-plugin instead
+      // 'no-extra-parens': 'off',
+      // '@typescript-eslint/no-extra-parens': ['error'],
 
-        // note you must disable the base rule as it can report incorrect errors
-        "no-magic-numbers": "off",
-        "@typescript-eslint/no-magic-numbers": [
-            "error",
-            {
-                ignoreEnums: true
-            }
-        ],
+      // note you must disable the base rule as it can report incorrect errors
+      'no-magic-numbers': 'off',
+      '@typescript-eslint/no-magic-numbers': [
+        'error',
+        {
+          ignoreEnums: true
+        }
+      ],
 
-        "@typescript-eslint/restrict-plus-operands": "error",
+      '@typescript-eslint/restrict-plus-operands': 'error',
 
-        // note you must disable the base rule as it can report incorrect errors
-        "semi": "off",
-        "@typescript-eslint/semi": ["error"],
+      // note you must disable the base rule as it can report incorrect errors
+      // Use @stylistic/semi
+      // 'semi': 'off',
+      // '@typescript-eslint/semi': ['error'],
 
-        "@typescript-eslint/no-explicit-any": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
 
-        "no-unused-vars": "off",
-        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+      '@typescript-eslint/no-empty-object-type': 'off',
+
+      'sonarjs/function-return-type': 'off',
     }
 
-};
+  }
+];
